6c2b517936b8b6981a63d6f322fbab08a1c095a6
OmChillure <omchillure4@gmail.com>
2024-06-07 12:58:02 +0530

    engine

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://127.0.0.1:8000/api/:path*'
-            : 'https://flaskss.vercel.app/api/:path*', // Ensure this is your Flask app's URL
+            : 'https://mediflask.vercel.app/api/:path*', // Ensure this is your Flask app's URL
       },
     ];
   },
